e50545a7abb355dcbb4a83c834288568423b09e3
Deezzir <deezzir@gmail.com>
2024-11-18 11:26:41 -0500

    Naming improvements

--- tests/functional/test_snap.py
+++ tests/functional/test_snap.py
@@ -11,24 +11,24 @@ SNAP_NAME = "smartctl-exporter"
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(10))
-def _check_service_failed() -> None:
-    """Check if a systemd service is in a failed state."""
+def _assert_service_failed() -> None:
+    """Assert the systemd service is in a failed state."""
     assert 0 == subprocess.call(
         f"sudo systemctl is-failed --quiet {SYSTEMD_SERVICE}".split()
     ), f"{SYSTEMD_SERVICE} is running"
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(10))
-def _check_service_active() -> None:
-    """Check if a systemd service is in a activemstate."""
+def _assert_service_active() -> None:
+    """Assert the systemd service is in a active state."""
     assert 0 == subprocess.call(
         f"sudo systemctl is-active --quiet {SYSTEMD_SERVICE}".split()
     ), f"{SYSTEMD_SERVICE} is not running"
 
 
 @retry(wait=wait_fixed(5), stop=stop_after_delay(30))
-def _check_endpoint(endpoint: str) -> None:
-    """Check if an endpoint is reachable."""
+def _assert_endpoint_reachable(endpoint: str) -> None:
+    """Assert the endpoint is reachable."""
     response = urllib.request.urlopen(endpoint)  # will raise if not reachable
     status_code = response.getcode()
     assert status_code == 200, f"Endpoint {endpoint} returned status code {status_code}"
@@ -42,16 +42,16 @@ def _get_endpoint_data(endpoint: str) -> str:
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(10))
-def _check_bind(bind: str, service: str) -> None:
-    """Check if a service is listening on a specific bind."""
+def _assert_service_listening(bind: str, service: str) -> None:
+    """Assert service is listening on a specific bind."""
     pid = subprocess.check_output(f"sudo lsof -t -i {bind}".split(), text=True).strip()
     assert service in subprocess.check_output(
         f"cat /proc/{pid}/cmdline".split(), text=True
-    ), f"{SNAP_NAME} is not listening on {bind}"
+    ), f"{service} is not listening on {bind}"
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(10))
-def _set_config(config_parent: str, config: str, value: str) -> None:
+def _set_snap_config(config_parent: str, config: str, value: str) -> None:
     """Set a configuration value for a snap service."""
     assert 0 == subprocess.call(
         f"sudo snap set {SNAP_NAME} {config_parent}.{config}={value}".split()
@@ -59,7 +59,7 @@ def _set_config(config_parent: str, config: str, value: str) -> None:
 
 
 @retry(wait=wait_fixed(2), stop=stop_after_delay(10))
-def _unset_config(config_parent: str, config: str) -> None:
+def _unset_snap_config(config_parent: str, config: str) -> None:
     """Unset a configuration value for a snap service."""
     assert 0 == subprocess.call(
         f"sudo snap unset {SNAP_NAME} {config_parent}.{config}".split()
@@ -67,10 +67,11 @@ def _unset_config(config_parent: str, config: str) -> None:
 
 
 def _get_start_cmd(process: str) -> str:
+    """Get the command used to start a process."""
     return subprocess.check_output(f"ps -C {process} -o cmd -ww".split(), text=True)
 
 
-def _check_config(config_parent: str, config: str):
+def _assert_config_exists(config_parent: str, config: str):
     """Check if a configuration exists in the snap configuration."""
     result = subprocess.check_output(f"sudo snap get {SNAP_NAME} -d".split(), text=True)
     smartctl_snap_config = json.loads(result.strip())
@@ -80,17 +81,17 @@ def _check_config(config_parent: str, config: str):
 
 
 @contextmanager
-def _config_setup(config_str, new_value):
+def _snap_config_ctx(config_str, new_value):
     """Set up a context manager to test snap configuration."""
     config_parent, config = config_str.split(".")
     try:
-        _check_config(config_parent, config)
-        _set_config(config_parent, config, new_value)
+        _assert_config_exists(config_parent, config)
+        _set_snap_config(config_parent, config, new_value)
         yield
     finally:
         # Revert back
-        _unset_config(config_parent, config)
-        _check_service_active()
+        _unset_snap_config(config_parent, config)
+        _assert_service_active()
 
 
 # Snap tests
@@ -98,8 +99,8 @@ def _config_setup(config_str, new_value):
 
 def test_smartctl_exporter_service() -> None:
     """Test of the smartctl_exporter service and its endpoint."""
-    _check_service_active()
-    _check_endpoint(ENDPOINT)
+    _assert_service_active()
+    _assert_endpoint_reachable(ENDPOINT)
 
 
 def test_smartctl_exporter_metrics() -> None:
@@ -111,20 +112,20 @@ def test_smartctl_exporter_metrics() -> None:
 def test_valid_bind_config() -> None:
     """Test valid snap bind configuration."""
     new_bind = ":9770"
-    with _config_setup("web.listen-address", new_bind):
-        _check_bind(new_bind, "smartctl_exporter")
+    with _snap_config_ctx("web.listen-address", new_bind):
+        _assert_service_listening(new_bind, "smartctl_exporter")
 
 
 def test_invalid_bind_config() -> None:
     """Test invalid snap bind configuration."""
-    with _config_setup("web.listen-address", "test"):
-        _check_service_failed()
+    with _snap_config_ctx("web.listen-address", "test"):
+        _assert_service_failed()
 
 
 def test_valid_log_level_config() -> None:
     """Test valid snap log level configuration."""
-    with _config_setup("log.level", "debug"):
-        _check_service_active()
+    with _snap_config_ctx("log.level", "debug"):
+        _assert_service_active()
         for attempt in Retrying(wait=wait_fixed(2), stop=stop_after_delay(10)):
             with attempt:
                 assert "log.level=debug" in _get_start_cmd(
@@ -134,15 +135,15 @@ def test_valid_log_level_config() -> None:
 
 def test_invalid_log_level_config() -> None:
     """Test invalid snap log level configuration."""
-    with _config_setup("log.level", "test"):
-        _check_service_failed()
+    with _snap_config_ctx("log.level", "test"):
+        _assert_service_failed()
 
 
 def test_include_device_config() -> None:
     """Test include device configuration."""
-    with _config_setup("smartctl.device-include", "/dev/test"):
-        _check_service_active()
-        # By giving an invalid device, the exporter should not find any devices
+    with _snap_config_ctx("smartctl.device-include", "/dev/test"):
+        _assert_service_active()
+        # By giving an invalid device, the exporter should not scan any devices
         assert "smartctl_devices 0" in _get_endpoint_data(ENDPOINT)
 
 
@@ -152,6 +153,6 @@ def test_exclude_device_config() -> None:
     result = subprocess.check_output("sudo lsblk -d -e7 -J".split(), text=True)
     device_name = json.loads(result)["blockdevices"][0]["name"]
 
-    with _config_setup("smartctl.device-exclude", device_name):
-        _check_service_active()
+    with _snap_config_ctx("smartctl.device-exclude", device_name):
+        _assert_service_active()
         assert device_name not in _get_endpoint_data(ENDPOINT)
